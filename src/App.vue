d06bd4638d5615a3d83fcd2b637b4ebdb664d7b7
艾伦 Aaron <401545553@qq.com>
2018-06-20 16:44:14 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+          <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+      </transition>
+      <transition name="router-fade" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
+    <svg-icon></svg-icon>
+    </div>
+</template>
+
+
+
+<style lang="scss">
+  .router-fade-enter-active, .router-fade-leave-active {
+      transition: opacity .3s;
+  }
+  .router-fade-enter, .router-fade-leave-active {
+      opacity: 0;
+  }
+</style>
