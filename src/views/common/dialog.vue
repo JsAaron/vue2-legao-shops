388e58129fddaa36e38a7e0a93bcd35345b59b18
艾伦 Aaron <401545553@qq.com>
2018-08-20 14:27:25 +0800

    更新css

--- src/views/common/dialog.vue
+++ src/views/common/dialog.vue
@@ -3,6 +3,7 @@
     <el-dialog :title="title" :visible.sync="visible" :before-close="dialogClose">
       <!-- 内容 -->
       <slot name="main"></slot>
+      <!-- 底部 -->
       <span slot="footer" class="dialog-footer">
         <slot name="footer"></slot>
       </span>
@@ -21,55 +22,3 @@ export default {
 };
 </script>
 
-<style lang="scss" >
-.dialog-common {
-  .el-dialog {
-    @include borderRadius(0.2rem);
-    // background: transparent;
-    margin-top: 0 !important;
-    @include setCenter;
-    .el-dialog__header {
-      border-top-left-radius: 0.2rem;
-      border-top-right-radius: 0.2rem;
-      background: #4b91cd;
-      position: relative;
-      .el-dialog__title {
-        @include setCenter;
-      }
-      .el-dialog__title,
-      .el-dialog__close {
-        font-size: 0.22rem;
-        color: #ffffff;
-      }
-    }
-    .el-dialog__body {
-      background: #ffffff;
-    }
-    .el-dialog__footer {
-      background: #ffffff;
-      border-bottom-left-radius: 0.2rem;
-      border-bottom-right-radius: 0.2rem;
-    }
-  }
-}
-
-.dialog-common {
-  .el-dialog__header {
-    height: 0.56rem;
-  }
-  .el-dialog__body {
-    width: auto !important;
-    .title {
-      text-align: center;
-    }
-    p {
-      padding-top: 0.1rem;
-      font-size: 0.2rem;
-    }
-  }
-  .el-dialog__footer {
-    display: flex;
-    justify-content: space-around;
-  }
-}
-</style>
