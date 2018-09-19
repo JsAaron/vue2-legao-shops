29c93370c1dec84c964b3c9ab9cb700b7f1f3b89
艾伦 Aaron <401545553@qq.com>
2018-09-19 16:23:59 +0800

    no message

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 - 乐高商铺端后台管理平台，分桌面端与手机端
 - 桌面端 vue2+webpack4+element-ui+rem
-- 手机端 vue2+webpack4+cube+vw/vh
+- 手机端 vue2+webpack4+vant+vw/vh
+- 桌面端采用 vw 定 html，单位用 rem, 控制区域
+- 手机端 采用 postcss 转化 px 为 vw/vh 单位
--- legao-desktop/src/config/rem.js
+++ legao-desktop/src/config/rem.js
@@ -14,3 +14,20 @@ function rem() {
   // var rem = wW * whdef; // 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
   // document.getElementsByTagName("html")[0].style.fontSize = rem + "px";
 }
+
+// 基准大小
+const baseSize = 100;
+// 设置 rem 函数
+function setRem() {
+  // 当前页面宽度相对于 750 宽的缩放比例，可根据自己需要修改。
+  const scale = document.documentElement.clientWidth / 1920;
+  // 设置页面根节点字体大小
+  document.documentElement.style.fontSize =
+    baseSize * Math.min(scale, 2) + "px";
+}
+// 初始化
+setRem();
+// 改变窗口大小时重新设置 rem
+window.onresize = function() {
+  setRem();
+};
