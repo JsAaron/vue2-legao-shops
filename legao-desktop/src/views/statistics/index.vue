3aabe0da48198d2cb5aa6b4047c44a3c5f862af5
艾伦 Aaron <401545553@qq.com>
2018-09-30 14:55:27 +0800

    no message

--- legao-desktop/src/views/statistics/index.vue
+++ legao-desktop/src/views/statistics/index.vue
@@ -1,36 +1,160 @@
 <template>
-  <div class="tab-container">
-    <el-tag>mounted times ：{{createdTimes}}</el-tag>
-    <el-alert style="width:200px;display:inline-block;vertical-align: middle;margin-left:30px;" title="Tab with keep-alive" type="success" :closable="false">
-    </el-alert>
-    <el-tabs style='margin-top:15px;' v-model="activeName" type="border-card">
-      <el-tab-pane v-for="item in tabMapOptions" :label="item.label" :key='item.key' :name="item.key">
-      </el-tab-pane>
-    </el-tabs>
+  <div class="statistics-container">
+
+    <h3>销售数据统计（元）</h3>
+    <div class="line"></div>
+    <div class="statistics-list">
+      <!-- 列表数据 -->
+      <div class="legao-list">
+          <el-table
+            :data="salesData"
+            tooltip-effect="dark">
+            <el-table-column
+              prop="name"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="7p"
+              label="7日销售额"
+              width="150"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="30p"
+              label="30日销售额"
+              width="150"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="360p"
+              label="年销售额"
+              width="150"
+              align="center">
+            </el-table-column>
+          </el-table>
+      </div>
+    </div>
+
+    <h3>会员卡销量统计（张）</h3>
+    <div class="line"></div>
+    <div class="statistics-list">
+      <!-- 列表数据 -->
+      <div class="legao-list">
+          <el-table
+            :data="cardData"
+            tooltip-effect="dark">
+            <el-table-column
+              prop="name"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="7p"
+              label="7天销量"
+              width="150"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="30p"
+              label="30天销量"
+              width="150"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="360p"
+              label="总销量"
+              width="150"
+              align="center">
+            </el-table-column>
+          </el-table>
+      </div>
+    </div>
+
+    <h3>店铺库存状态统计（件）</h3>
+    <div class="line"></div>
+    <div class="statistics-text">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <label>店铺玩:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>店铺展示:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>店铺可租借:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>借出在外:</label><span>80</span>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <label>会员借出已还:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>待清点分拣:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>清点分拣中:</label><span>80</span>
+        </el-col>
+        <el-col :span="6">
+          <label>店铺归还总部:</label><span>80</span>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "tab",
-  components: { tabPane },
   data() {
     return {
-      tabMapOptions: [
-        { label: "China", key: "CN" },
-        { label: "USA", key: "US" },
-        { label: "Japan", key: "JP" },
-        { label: "Eurozone", key: "EU" }
+      salesData: [
+        { name: "交易金额", "7p": 11111, "30p": 11111, "360p": 11111 },
+        { name: "押金金额", "7p": 33333, "30p": 11111, "360p": 11111 }
       ],
-      activeName: "CN",
-      createdTimes: 0
+      cardData: [
+        { name: "月卡", "7p": 1, "30p": 1, "360p": 1 },
+        { name: "月卡1", "7p": 1, "30p": 1, "360p": 1 },
+        { name: "月卡2", "7p": 1, "30p": 1, "360p": 1 },
+        { name: "月卡3", "7p": 1, "30p": 1, "360p": 1 },
+        { name: "月卡4", "7p": 1, "30p": 1, "360p": 1 },
+        { name: "月卡7", "7p": 1, "30p": 1, "360p": 1 }
+      ]
     };
-  }
+  },
+  methods: {}
 };
 </script>
 
-<style scoped>
-.tab-container {
-  margin: 30px;
+<style lang="scss" scoped>
+.statistics-container {
+  > h3 {
+    margin-top: 0.3rem;
+    font-size: 0.24rem;
+  }
+  padding-left: 2rem;
+  .line {
+    margin: 0.2rem 0;
+    width: 0.49rem;
+    height: 0.07rem;
+    background-color: #efb526;
+  }
+  .statistics-list {
+    width: 8.39rem;
+    .legao-list {
+      width: 100%;
+    }
+  }
+  .statistics-text {
+    margin-top: 0.3rem;
+    width: 10rem;
+    .el-row {
+      margin-top: 0.2rem;
+    }
+    label,
+    span {
+      font-size: 0.18rem;
+    }
+  }
 }
 </style>
